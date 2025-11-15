7412f68acde6644b4598440549cbf529533b3feb
Marcel de Vries <vriesmarcel@hotmail.com>
2025-11-15 14:41:38 +0100

    :ambulance: reset the started file

--- exercises/module3/start/ChatWithAgent.cs
+++ exercises/module3/start/ChatWithAgent.cs
@@ -23,11 +23,6 @@ namespace modulerag
         public async Task let_agent_find_ride(string deploymentName, string endpoint, string apiKey)
         {
             Console.WriteLine("******** Create the kernel ***********");
-            IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
-            var credential = new AzureKeyCredential(apiKey);
-            kernelBuilder.AddOpenAIChatCompletion(deploymentName,new Uri(endpoint), apiKey);
-            Kernel kernel = kernelBuilder.Build();
-            
             Console.WriteLine("******** Create the agent ***********");
             Console.WriteLine("******** Start the agent ***********");
             Console.WriteLine("******** RESPONSE ***********");
