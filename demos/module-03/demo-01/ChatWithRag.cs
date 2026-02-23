48d37196861e72e454cb7f2913c942e33c8bd181
Hans Bakker <hansmbakker+github@gmail.com>
2026-02-23 10:13:30 +0000

    Fix structured output syntax and make according to Agent Framework RC1

--- demos/module-03/demo-01/ChatWithRag.cs
+++ demos/module-03/demo-01/ChatWithRag.cs
@@ -17,7 +17,7 @@ public class ChatWithRag
         """;
 
         //var policyContext = "";
-        //var policyContext = File.ReadAllText("../../../../datasets/venue-policies/AFAS_Live.md");
+        //var policyContext = File.ReadAllText("../datasets/venue-policies/AFAS_Live.md");
 
         var venue = await GetVenueFromQuestion(chatClient, question);
         var policyContext = await GetVenuePolicyFileContents(chatClient, venue);
@@ -29,35 +29,23 @@ public class ChatWithRag
     private async Task<string> GetVenuePolicyFileContents(IChatClient chatClient, string venueName)
     {
         //Get a list of files from the venue policy repository
-        var directory = "../../../../datasets/venue-policies";
-        var fileList = string.Join("\n", System.IO.Directory.GetFiles(directory, "*.md").Select(f => System.IO.Path.GetFileName(f)));
+        var directory = "../datasets/venue-policies";
+        var fileList = string.Join("\n", Directory.GetFiles(directory, "*.md").Select(f => Path.GetFileName(f)));
 
         var systemPrompt = "You are an expert at finding the correct file based on a user question.";
         var fileListPrompt = $"The following is a list of files available:\n{fileList}";
         var fileQuestion = $"Which file contains the venue policy for the venue named '{venueName}'?";
 
-        var messages = new List<ChatMessage>();
+        IEnumerable<ChatMessage> messages = [
+            new ChatMessage(ChatRole.System, systemPrompt),
+            new ChatMessage(ChatRole.User, fileListPrompt),
+            new ChatMessage(ChatRole.User, fileQuestion)
+        ];
 
-        messages.Add(new ChatMessage(ChatRole.System, systemPrompt));
-        messages.Add(new ChatMessage(ChatRole.User, fileListPrompt));
-        messages.Add(new ChatMessage(ChatRole.User, fileQuestion));
+        AIAgent agent = chatClient.AsAIAgent();
 
-        JsonElement schema = AIJsonUtilities.CreateJsonSchema(typeof(SelectedFile));
-
-        var chatOptions = new ChatOptions()
-        {
-            ResponseFormat = ChatResponseFormat.ForJsonSchema(
-                                schema: schema,
-                                schemaName: nameof(SelectedFile),
-                                schemaDescription: "An object containing the full file name of a file")
-        };
-
-        AIAgent agent = chatClient.AsAIAgent(
-            new ChatClientAgentOptions() { ChatOptions = chatOptions });
-
-
-        var response = await agent.RunAsync(messages);
-        var fileResult = response.Deserialize<SelectedFile>(JsonSerializerOptions.Web);
+        var response = await agent.RunAsync<SelectedFile>(messages, serializerOptions: JsonSerializerOptions.Web);
+        var fileResult = response.Result;
         
         var fullFileName = Path.Combine(directory, fileResult.File);
 
@@ -74,27 +62,16 @@ public class ChatWithRag
 
     private async Task<string> GetVenueFromQuestion(IChatClient chatClient, string question)
     {
-        var messages = new List<ChatMessage>();
+        IEnumerable<ChatMessage> messages = [
+            new ChatMessage(ChatRole.System, "You are a helpful asistant that finds the name of a venue from a question."),
+            new ChatMessage(ChatRole.System, "Always get the information from the question. Never search the web or use internal knowledge!"),
+            new ChatMessage(ChatRole.User, question)
+        ];
 
-        messages.Add(new ChatMessage(ChatRole.System, "You are a helpful asistant that finds the name of a venue from a question."));
-        messages.Add(new ChatMessage(ChatRole.System, "Always get the information from the question. Never search the web or use internal knowledge!"));
-        messages.Add(new ChatMessage(ChatRole.User, question));
+        AIAgent agent = chatClient.AsAIAgent();
 
-        JsonElement schema = AIJsonUtilities.CreateJsonSchema(typeof(SelectedVenue));
-
-        var chatOptions = new ChatOptions()
-        {
-            ResponseFormat = ChatResponseFormat.ForJsonSchema(
-                                schema: schema,
-                                schemaName: nameof(SelectedVenue),
-                                schemaDescription: "An object containing the name of a venue")
-        };
-
-        AIAgent agent = chatClient.AsAIAgent(
-            new ChatClientAgentOptions() { ChatOptions = chatOptions });
-
-        var response = await agent.RunAsync(messages);
-        var selectedVenue = response.Deserialize<SelectedVenue>(JsonSerializerOptions.Web);
+        var response = await agent.RunAsync<SelectedVenue>(messages, serializerOptions: JsonSerializerOptions.Web);
+        var selectedVenue = response.Result;
         return selectedVenue!.VenueName;
     }
 
@@ -102,12 +79,12 @@ public class ChatWithRag
     {
         AIAgent agent = chatClient.AsAIAgent();
 
-        var messages = new List<ChatMessage>();
-        messages.Add(new ChatMessage(ChatRole.System, "You are a helpful assistant that answers questions from people that go to a concert and have questions about the venue.")); ;
-        messages.Add(new ChatMessage(ChatRole.System, "Always use the policy information provided in the prompt"));
-        messages.Add(new ChatMessage(ChatRole.System, $"### Venue Policy\n {policyContext}"));
-
-        messages.Add(new ChatMessage(ChatRole.User, question));
+        IEnumerable<ChatMessage> messages = [
+            new ChatMessage(ChatRole.System, "You are a helpful assistant that answers questions from people that go to a concert and have questions about the venue."),
+            new ChatMessage(ChatRole.System, "Always use the policy information provided in the prompt"),
+            new ChatMessage(ChatRole.System, $"### Venue Policy\n {policyContext}"),
+            new ChatMessage(ChatRole.User, question)
+        ];
 
         var questionResponse = agent.RunStreamingAsync(messages);
         await foreach (var response in questionResponse)
