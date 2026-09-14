7bbe35a73fca97b1761968b777300c712c0ad443
Roy Cornelissen <ext_roy.cornelissen@jotron.com>
2026-09-13 18:03:17 -0700

    ✨ Implement module-02 exercise-04 completed

---
+++ exercises/module2/completed/exercise-04/MyContextProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+
+namespace AgentFramework101;
+
+internal class MyContextProvider: AIContextProvider
+{
+    protected override ValueTask<AIContext> ProvideAIContextAsync(InvokingContext context, CancellationToken cancellationToken = default)
+    {
+        var aiContext = new AIContext()
+        {
+            Instructions = "Always answer like you are a pirate. Arrr.",
+            Messages = [new ChatMessage(ChatRole.User, "My favorite genre is rock music.")]
+        };
+        return ValueTask.FromResult(aiContext);
+    }
+}
